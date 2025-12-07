458e26fc16e67f2cefd544628b6b5d051c2366b0
wegnerdamian <wedegdy@gmail.com>
2025-12-07 15:23:16 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // WAŻNE: Musi być './App.jsx', a nie './gildiatrenerow'
+import './index.css' // Jeśli masz ten plik, jeśli nie - usuń tę linię, ale Tailwind jest ładowany w index.html
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

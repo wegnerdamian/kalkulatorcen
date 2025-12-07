eb668be156386bbcd575221aa1832e52c2fad0c0
wegnerdamian <wedegdy@gmail.com>
2025-12-07 16:40:23 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// Usunęliśmy linię importu CSS, ponieważ style ładujemy w index.html
-// import './index.css' 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
